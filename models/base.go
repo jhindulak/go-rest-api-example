133263978f9b1dc065292c63af40051e09c5d275
Jason Hindulak <jason.hindulak@hyland.com>
2019-08-30 15:36:48 -0400

    Adding gorm db_port

--- models/base.go
+++ models/base.go
@@ -22,9 +22,14 @@ func init() {
 	password := os.Getenv("MASTER_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("ENDPOINT_ADDRESS")
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+		fmt.Println("Using default DB port 5432.")
+	}
 
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s",
-		dbHost, username, dbName, password)
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		dbHost, dbPort, username, dbName, password)
 	fmt.Println(dbUri)
 
 	conn, err := gorm.Open("postgres", dbUri)

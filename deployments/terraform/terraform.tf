8f5dae0464347578416e3dffe4ff57b93a7a2f0b
Jason Hindulak <jason.hindulak@hyland.com>
2019-09-25 16:26:07 -0400

    Testing environment variable locations

--- deployments/terraform/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12.0"
-
-  backend "s3" {
-    bucket         = "go-contacts-terraform"
-    region         = "us-east-1"
-    dynamodb_table = "go-contacts-terraformStateLock"
-  }
-}
